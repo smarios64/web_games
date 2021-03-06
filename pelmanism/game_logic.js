d0a676d660551dfea0c84a668183b408d84172d8
Marios Christoforakis <smarios64@gmail.com>
2021-03-06 15:27:43 +0200

    Added visual and audio feedback for pelmanism.

--- pelmanism/game_logic.js
+++ pelmanism/game_logic.js
@@ -12,13 +12,17 @@
         'orange'
     ];
 
-    var deck = []; // this should never change after initialization
-    var covers = []; // this should never change after initialization
-    var pairs = [];
+    const deck = []; // this should never change after initialization
+    const covers = []; // this should never change after initialization
+    const pairs = [];
+    const correctAudio = new Audio('./assets/audio/correct.mp3');;
+    const wrongAudio = new Audio('./assets/audio/wrong.mp3');;
+    const congratsAudio = new Audio('./assets/audio/congratulations.mp3');;
     var openCard = -1;
 
-    var reset = function() {
-        var temp = [];
+
+    const reset = function() {
+        const temp = [];
         openCard = -1;
         gameOverScreen.style.display = 'none';
 
@@ -34,28 +38,29 @@
         while (temp.length > 0) {
             var index = Math.floor(Math.random() * temp.length);
             pairs[counter] = temp[index];
-            temp[index].style.display = 'block';
-            cardPHs[counter].appendChild(temp[index]);
+            temp[index].card.style.display = 'block';
+            cardPHs[counter].appendChild(temp[index].card);
             temp.splice(index, 1);
             ++counter;
         }
         startBtn.style.display = 'block';
     }
 
-    var onCardClicked = function(e) {
-        var index = covers.indexOf(e.currentTarget);
-        pairs[index].style.display = 'block';
+    const onCardClicked = function(e) {
+        const index = covers.indexOf(e.currentTarget);
+        pairs[index].card.style.display = 'block';
         covers[index].style.display = 'none';
         if (openCard < 0) {
             openCard = index;
         }
         else {
             // did we find a match?
-            if (pairs[index].style.backgroundColor === pairs[openCard].style.backgroundColor) {
+            if (pairs[index].card.style.backgroundColor === pairs[openCard].card.style.backgroundColor) {
                 // remove the cards from pairs[]
+                var openCards = [pairs[openCard].card, pairs[index].card];
                 pairs[openCard] = pairs[index] = undefined;
-
-                // TODO: play success sound
+                correctAudio.play();
+                openCards[0].style.boxShadow = openCards[1].style.boxShadow = '0 0 5px 5px #0f8';
 
                 // check if we ran out of pairs and if so, game over
                 var gameOver = true;
@@ -65,27 +70,28 @@
                         break;
                     }
                 }
-                if (gameOver) {
-                    setTimeout(function() {
-                        // TODO: play applause sound
+                setTimeout(function() {
+                    if (gameOver) {
+                        congratsAudio.play();
                         gameOverScreen.style.display = 'block';
-                    }, 1000);
-                }
+                    }
+                    openCards[0].style.boxShadow = openCards[1].style.boxShadow = '0 0 6px black';
+                }, 1000);
             }
             else {
-                // TODO: play fail sound
+                wrongAudio.play();
+                pairs[openCard].card.style.boxShadow = pairs[index].card.style.boxShadow = '0 0 5px 5px red';
 
                 // remove events temporarily
                 for (var i = 0; i < covers.length; ++i) {
                     covers[i].onclick = undefined;
                 }
                 // flip cards back down
-                var tempCard = openCard;
+                const tempCard = openCard;
                 setTimeout(function() {
-                    pairs[tempCard].style.display = 'none';
-                    pairs[index].style.display = 'none';
-                    covers[tempCard].style.display = 'block';
-                    covers[index].style.display = 'block';
+                    pairs[tempCard].card.style.boxShadow = pairs[index].card.style.boxShadow = '0 0 6px black';
+                    pairs[tempCard].card.style.display = pairs[index].card.style.display = 'none';
+                    covers[tempCard].style.display = covers[index].style.display = 'block';
                     for (var i = 0; i < covers.length; ++i) {
                         covers[i].onclick = onCardClicked;
                     }
@@ -105,7 +111,7 @@
         tableNodes += "</tr>";
     }
     document.getElementById('deckTable').innerHTML = tableNodes;
-    var cardPHs = document.getElementsByTagName("td"); // this is safe as we currently do not have any tables besides the deck table
+    const cardPHs = document.getElementsByTagName("td"); // this is safe as we currently do not have any tables besides the deck table
 
     for (var i = 0; i < cardPHs.length; ++i) {
         var cover = document.createElement('div');
@@ -119,7 +125,7 @@
     for (var i = 0; i < COLORS.length; ++i) {
         for (var j = 0; j < 2; ++j) {
             var card = document.createElement('div');
-            deck.push(card);
+            deck.push({ card: card, audio: 'assets/audio/card_' + i + '.mp3' });
             card.className = 'card';
             card.style.backgroundColor = COLORS[i];
             card.innerHTML = '<img src="assets/images/card_' + i + '.png">'
@@ -130,14 +136,14 @@
     var startBtn = document.getElementById("startBtn");
     startBtn.onclick = function() {
         for (var i = 0; i < pairs.length; ++i) {
-            pairs[i].style.display = 'none';
+            pairs[i].card.style.display = 'none';
             covers[i].style.display = 'block';
         }
         startBtn.style.display = 'none';
     };
     
     document.getElementById("playAgainBtn").onclick = reset;
-    var gameOverScreen = document.getElementById("gameOverScreen");
+    const gameOverScreen = document.getElementById("gameOverScreen");
 
     // TODO: play music
 
