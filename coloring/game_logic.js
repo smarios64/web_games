265990db24c290bc6d1715aad55f0206c5e02aed
Marios Christoforakis <smarios64@gmail.com>
2021-04-02 20:54:58 +0300

    Added new image for coloring game and option to load image on demand via url params.

--- coloring/game_logic.js
+++ coloring/game_logic.js
@@ -1,14 +1,17 @@
 (function() {
     "use strict"
 
-    const TOTAL_DRAWINGS = 1;
+    const TOTAL_DRAWINGS = 2; // don't forget to increment as you add new images
     const drawing = document.getElementById("main_image");
-    const colors = document.getElementsByClassName("palette");
-
+    const colors = document.getElementsByClassName("color_pick");
     var currentColor = colors[0];
+    var imgIndex = Number.parseInt(location.href.substr(location.href.indexOf('?') + 1)); // select an image based on url params 
+    if (Number.isNaN(imgIndex) || imgIndex >= TOTAL_DRAWINGS) { // in case we fail to parse the url params, load a random image
+        imgIndex = Math.floor(Math.random() * TOTAL_DRAWINGS);
+    }
 
     function selectColor() {
-        currentColor.style.border = "5px solid black";
+        currentColor.style.border = "3px solid #bbb";
         currentColor = this;
         currentColor.style.border = "8px solid #0f0";
     }
@@ -29,5 +32,5 @@
     }
 
     selectColor.call(colors[0]);
-    drawing.data = "./assets/drawing_" + Math.floor(Math.random() * TOTAL_DRAWINGS) + ".svg";
+    drawing.data = "./assets/drawing_" + imgIndex + ".svg";
 })();
